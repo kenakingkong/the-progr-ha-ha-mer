d9b4c192c87142cb32bd96cda2ec3a479e09436f
Makena Kong <kena.kong@gmail.com>
2023-07-26 02:03:54 -0700

    fixed base url

--- src/utils/appinfo.ts
+++ src/utils/appinfo.ts
@@ -1,8 +1,7 @@
 namespace AppInfoUtils {
   export const TITLE = "THE PROGR-HA-HA-MER";
   export const DESCRIPTION = "Clean & silly programmer jokes.";
-  export const URL =
-    "https://the-progr-ha-ha-cl6df8r52-kenakingkong.vercel.app";
+  export const URL = "https://the-progr-ha-ha-mer.vercel.app/";
   export const IMAGE = "/public/unfurl.webp";
 }
 
