653568f101786f63917765aedfcf1ad052d2d360
Makena Kong <kena.kong@gmail.com>
2023-07-27 00:40:40 -0700

    fix unfurl img

--- src/utils/appinfo.ts
+++ src/utils/appinfo.ts
@@ -2,7 +2,7 @@ namespace AppInfoUtils {
   export const TITLE = "THE PROGR-HA-HA-MER";
   export const DESCRIPTION = "Clean & silly programmer jokes.";
   export const URL = "https://the-progr-ha-ha-mer.vercel.app/";
-  export const IMAGE = "/unfurl.webp";
+  export const IMAGE = "https://the-progr-ha-ha-mer.vercel.app/unfurl.webp";
 }
 
 export default AppInfoUtils;
